877317b85c8b5661ada123d1d7258be25691d13d
Nagesh Martha <nagesh@MacBook-Pro.local>
2020-07-29 23:36:56 -0400

    Updates

---
+++ precache-manifest.3c987178e7a2d781ada7d1b90e331d28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0278872bc3b02cbd26d8f419ee960a91",
+    "url": "/Places/index.html"
+  },
+  {
+    "revision": "71b25dcc1a8b090add5d",
+    "url": "/Places/static/css/main.fd04ebc7.chunk.css"
+  },
+  {
+    "revision": "5b40f112e24ae1429baa",
+    "url": "/Places/static/js/2.4ee4fd8b.chunk.js"
+  },
+  {
+    "revision": "71b25dcc1a8b090add5d",
+    "url": "/Places/static/js/main.7e681d4c.chunk.js"
+  },
+  {
+    "revision": "6c9a4f742ee99f09320a",
+    "url": "/Places/static/js/runtime-main.5e190765.js"
+  }
+]);
